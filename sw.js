a07697a290066533cbc83d7c93b77785b2d449dc
daniel ordonez arango <danielordonezarango@gmail.com>
2026-06-12 00:33:50 +0000

    ui: mobile UX improvements and CSS fixes

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var CACHE = 'danieloa-20260612-b0c0fa4-003048';
+var CACHE = 'danieloa-20260612-f7fbb3f-003350';
 var ASSETS = [
   '/',
-  '/style.css?v=20260612-b0c0fa4-003048',
-  '/translations.js?v=20260612-b0c0fa4-003048',
-  '/script.js?v=20260612-b0c0fa4-003048',
+  '/style.css?v=20260612-f7fbb3f-003350',
+  '/translations.js?v=20260612-f7fbb3f-003350',
+  '/script.js?v=20260612-f7fbb3f-003350',
   '/daniel_profile.webp',
   '/manifest.json',
   '/favicon.svg',
